472a261d05c66ed2facd6609a37df139a1d61d6b
adithjaison <adithjaison@gmail.com>
2024-11-29 11:23:39 +0000

    commit section7

---
+++ app/app/calc.py
@@ -0,0 +1,20 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """
+    Add x and y return result
+    :param x:
+    :param y:
+    :return:
+    """
+    return x + y
+
+
+def subtract(x, y):
+    """
+    Subtract x from y and return result.
+    """
+    return y - x
